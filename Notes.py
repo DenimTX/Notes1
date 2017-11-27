35f78b4dad2d7c5406789e94e1d586010ceba951
DenimTX <31050506+DenimTX@users.noreply.github.com>
2017-11-27 15:39:31 -0800

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,26 @@
+print("Hello World")
+
+# Denim Xiong
+
+print()  # Creates a blank line
+print("See if you can figure this out")
+print(10 % 3)
+
+# Variables
+car_name = "Lamborguieapig"
+car_type = "Beast"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline Printing
+print("My car is the %s." % car_name)
+print("My car is the %s. It is a %s" % (car_name, car_type))
+# Taking input
+name = input("What is your name? ")
+print("Hello %s." % name)
+# print(name)
+
+age = input("How old are you? ")
+print("Oh, so you're %s years old." % age)
+# print(age)
+
